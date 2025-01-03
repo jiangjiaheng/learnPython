8d5edbe084dd777d6fcf0e656512d422f4ca109d
landscape <jiangjiaheng1994@qq.com>
2025-01-03 15:50:38 +0800

    add more content in hello

--- basicLearning/hello.py
+++ basicLearning/hello.py
@@ -1,1 +1,13 @@
-print("dada2")
+print("hello" + " world" + "!")
+print('cat\'s color')
+print("hello\nworld")
+print("""
+    hello
+    world
+    hope you everything
+    is okay
+""")
+
+my_love = 123
+print(f"my love num is {my_love}")
+
