0aea343fa6aa712f86ea42f69c6bc157dc5c1bc1
landscape <jiangjiaheng1994@qq.com>
2025-01-04 12:49:42 +0800

    add new content in hello

--- basicLearning/hello.py
+++ basicLearning/hello.py
@@ -1,13 +1,26 @@
-print("hello" + " world" + "!")
-print('cat\'s color')
-print("hello\nworld")
-print("""
-    hello
-    world
-    hope you everything
-    is okay
-""")
+# print("hello" + " world" + "!")
+# print('cat\'s color')
+# print("hello\n world")
+# print("""
+#     hello
+#     world
+#     hope you everything
+#     is okay
+# """)
 
-my_love = 123
-print(f"my love num is {my_love}")
+# my_love = 123
+# print(f"my love num is {my_love}")
 
+import math
+
+# result = math.log2(8)
+# print(result)
+
+a = -1
+b = -2
+c = 3
+delta = b ** 2 - 4 * a * c
+result_a = (-b + math.sqrt(delta)) / (2 * a)
+result_b = (-b - math.sqrt(delta)) / (2 * a)
+
+print(result_a, result_b)
